94961cfc1f2cb2dcb93762c31118dc4da4b89e8d
unknown <lahiruhiranwijesooriyansbm@gmail.com>
2020-05-26 19:27:18 +0530

    add the images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
+// import Cards from './components/Cards/Cards'
+// import Chart from "./components/Chart/Chart";
+// import CountryPicker from "./components/CountryPicker/CountryPicker";
+
+import {Cards,Chart,CountryPicker} from './components';
+import styles from "./App.module.css";
+import {fetchData} from './api/index'
 
 class App extends React.Component{
+  componentDidMount()
+  {
+    const data = await fetchData()
+  }
 render()
 {
-  return(
-    <div>
-      <h1>App</h1>
+  return (
+    <div class={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
-  )
+  );
 }
    
 }
---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const url = "https://covid19.mathdro.id/api";
+
+const fetchData = async ()=>
+{
+    try  
+    {
+        const response = await axios.get(url);
+        return response;
+        console.log(response);
+    }
+    catch(error)
+    {
+
+    }
+}
